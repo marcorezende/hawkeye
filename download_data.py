0448d761312beb9fb776ec8ebd26eeb2fe5f8935
Marco Rezende <marco.rezende@fpf.br>
2025-10-24 19:17:33 -0400

    script de adicao de filtro e download de imagens 2

--- download_data.py
+++ download_data.py
@@ -1,3 +1,4 @@
+import boto3
 from playwright.sync_api import sync_playwright
 import time
 import os
@@ -26,15 +27,58 @@ def main():
         page.fill("#mat-input-0", password)
         page.click("button:has-text('Entrar')")
 
-        time.sleep(10)
+        page.wait_for_url("https://app.checklistfacil.com.br/**", timeout=60000)
         page.goto("https://app.checklistfacil.com.br/evaluations")
         page.wait_for_selector("#start_date input.mdc-text-field__input")
 
-        page.fill("#start_date input.mdc-text-field__input", "21/09/2025")
-        page.fill("#end_date input.mdc-text-field__input", "28/09/2025")
-        page.click("button:has-text('Filtrar')")
+        page.fill("#start_date input.mdc-text-field__input", "26/09/2025")
+        page.keyboard.press("Tab")
 
-        time.sleep(5)
+        page.fill("#end_date input.mdc-text-field__input", "28/09/2025")
+        page.keyboard.press("Tab")
+
+        page.click("button:has-text('Filtrar') >> text='Filtrar'")
+        time.sleep(30)
+
+        page.click("#button-bulk-export")
+        page.get_by_role("radio", name="CSV").check()
+        page.locator("#export_bulk_evaluation_type_csv").get_by_role("textbox").click()
+        page.locator('li[data-value="evaluation_row_items_csv"]:visible').click()
+        page.get_by_role("button", name="Exportar").click()
+        time.sleep(30)
+        page.get_by_role("link", name="import_export").click()
+        time.sleep(20)
+
+        first_row = page.locator("table.data-table tbody tr:first-child")
+
+        first_row.hover()
+
+        page.wait_for_selector("table.data-table tbody tr:first-child a.js-row-download-button:visible")
+
+        first_download_button = page.locator("table.data-table tbody tr:first-child a.js-row-download-button:visible")
+
+        with page.expect_download() as download_info:
+            first_download_button.click()
+
+        download = download_info.value
+        download_path = os.path.join(download_dir, download.suggested_filename)
+        download.save_as(download_path)
+        print("Arquivo baixado para:", download.path())
+
+        s3_client = boto3.client(
+            "s3",
+            endpoint_url=os.getenv('MINIO_ENDPOINT'),
+            aws_access_key_id=os.getenv('MINIO_ACCESS_KEY'),
+            aws_secret_access_key=os.getenv('MINIO_SECRET_KEY'),
+            region_name="us-east-1",
+        )
+        #
+        local_file_path = download_path
+
+        object_name = os.path.basename(local_file_path)
+
+        s3_client.upload_file('downloads/avaliacoes-itens-por-linha-lm-da-silva-717804-20251022111833.csv', os.getenv('MINIO_BUCKET'), f'landing/{object_name}')
+        print(f"Arquivo enviado para MinIO: s3://{os.getenv('MINIO_BUCKET')}/landing/{object_name}")
 
 
 if __name__ == "__main__":
