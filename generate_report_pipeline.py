138cb3b23da3ca80cb5215f38f959d38535031a4
Marco Rezende <marco.rezende@fpf.br>
2025-11-12 22:13:48 -0400

    add streamlit

--- generate_report_pipeline.py
+++ generate_report_pipeline.py
@@ -1,14 +1,11 @@
 from prefect import flow
-from prefect import variables
 
 from superset_client import generate_report_pipe
 
 
 @flow
 def fluxo_principal(company, start_date, end_date):
-    # generate_report_pipe()
-    print(company)
-    # print(variables.get("company"))
+    generate_report_pipe(company, start_date, end_date)
 
 
 if __name__ == "__main__":
