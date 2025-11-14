bf2800f3faa2cc3ae956ba6aee6e159c8f992f8d
Marco Rezende <marco.rezende@fpf.br>
2025-11-13 20:07:41 -0400

    add streamlit

--- generate_report_pipeline.py
+++ generate_report_pipeline.py
@@ -4,8 +4,8 @@ from superset_client import generate_report_pipe
 
 
 @flow
-def fluxo_principal(company, start_date, end_date):
-    generate_report_pipe(company, start_date, end_date)
+def fluxo_principal(company, start_date, end_date, report_name):
+    generate_report_pipe(company, start_date, end_date, report_name)
 
 
 if __name__ == "__main__":
